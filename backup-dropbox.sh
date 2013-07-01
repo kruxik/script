30f3652a5d98e3afcad1f77fa1015250928bee00
krux <krux@livesport.eu>
2013-07-02 00:58:33 +0200

    Create a backup dir if not exists #noticket

--- backup-dropbox.sh
+++ backup-dropbox.sh
@@ -3,7 +3,13 @@
 cd ~/Dropbox
 DIRS=`find . -mindepth 1 -maxdepth 1 -type d | egrep -v "(\/Camera Uploads$|\/Photos$|\/.dropbox.cache$)"`
 DATE=`date +%Y-%m-%d`
-FILE="/home/krux/backup/Dropbox/dropbox-$DATE.tar.bz2"
+BACKUPDIR="/home/krux/backup/Dropbox"
+FILE="$BACKUPDIR/dropbox-$DATE.tar.bz2"
+
+if [ ! -d $BACKUPDIR ]; then
+	echo "Creating backup dir ..."
+	mkdir -p $BACKUPDIR
+fi
 
 echo "Archiving Dropbox directories ..."
 tar -cjf $FILE $DIRS
